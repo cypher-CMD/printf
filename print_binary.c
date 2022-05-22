b6945d2545f2442e6d9f95e7c30487db7212de32
Godwin Enahoro <giogdxgas1@gmail.com>
2022-05-22 15:37:41 -0700

    print Char edit

--- print_binary.c
+++ print_binary.c
@@ -7,10 +7,10 @@
  */
 void _putbase(char *str)
 {
-int i;
+	int i;
 
-for (i = 0; str[i] != '\0'; i++)
-_putcharacter(str[i]);
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
 }
 
 
@@ -21,25 +21,25 @@ _putcharacter(str[i]);
  */
 int _print_binary(va_list list)
 {
-int num, i, len;
-char *str;
+	int num, i, len;
+	char *str;
 
-num = va_arg(list, unsigned int);
-len = binary_len(num);
-str = malloc(sizeof(char) * len + 1);
-if (str == NULL)
-return (1);
-for (i = 0; num > 0; i++)
-{
-if (num % 2 == 0)
-str[i] = '0';
-else
-str[i] = '1';
-num = num / 2;
-}
-str[i] = '\0';
-rev_string(str);
-_putbase(str);
-free(str);
-return (len);
+	num =  va_arg(list, unsigned int);
+	len = binary_len(num);
+	str = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+		return (1);
+	for (i = 0; num > 0; i++)
+	{
+		if (num % 2 == 0)
+			str[i] = '0';
+		else
+			str[i] = '1';
+		num = num / 2;
+	}
+	str[i] = '\0';
+	rev_string(str);
+	_putbase(str);
+	free(str);
+	return (len);
 }
